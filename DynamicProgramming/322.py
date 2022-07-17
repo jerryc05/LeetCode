1859c3f6db2ac24caf7aec3ebdc6826884d4acbe
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-07-17 04:16:00 -0400

    upd 322

--- DynamicProgramming/322.py
+++ DynamicProgramming/322.py
@@ -1,50 +1,29 @@
-from itertools import chain
-
-
 class Solution:
-    dp: 'list[list[int]]|None' = None  # [coins, amount]
-    coins: 'list[int]|None' = None
-
     def coinChange(self, coins: list[int], amount: int) -> int:
-        self.dp = [[-2 for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
-        self.coins = coins
-        return self.f(0, amount)
+        if amount == 0:
+            return 0
 
-    # def f(self, coin_i: int, amount: int) -> int:
-    #     dp = self.dp
-    #     coins = self.coins
-    #     assert dp and coins
+        dp = [amount + 1] * (amount + 1)
+        #     |              |- [dp[amount]] shall be valid
+        #     |- cannot have more than [amount] coins; [amount+1] must be more than enough
 
-    #     if amount == 0:
-    #         return 1 if coin_i == len(coins) else 0
-    #     if amount < 0 or coin_i >= len(coins):
-    #         return -1
+        for c in coins:
+            if c < len(dp):
+                dp[c] = 1
 
-    #     if dp[coin_i][amount] == -2:
-    #         print(f'f({coin_i}, {amount})')
+        for a in range(amount + 1):
+            for c in coins:
+                if c < a:
+                    dp[a] = min(dp[a], dp[a - c] + 1)
 
-    #         num_coins = tuple(
-    #             filter(
-    #                 lambda x: x >= 0,
-    #                 chain(
-    #                     (self.f(i, amount - coins[i]) for i in range(len(coins))),
-    #                     (self.f(i + 1, amount - coins[i]) for i in range(len(coins))),
-    #                 ),
-    #             )
-    #         )
-
-    #         dp[coin_i][amount] = -1
-    #         if num_coins:
-    #             dp[coin_i][amount] = min(num_coins)+1
-
-    #     return dp[coin_i][amount]
+        return dp[amount] if dp[amount] != amount + 1 else -1
 
 
 f = Solution().coinChange
 
 
-# print(f([1, 2, 5], 11))
-# print(f([2], 3))
-# print(f([1, 2, 5], 0))
-# print(f([186, 419, 83, 408], 6249))
-print(f([346,29,395,188,155,109],9401))
+print(f([1, 2, 5], 11))
+print(f([2], 3))
+print(f([1, 2, 5], 0))
+print(f([186, 419, 83, 408], 6249))
+print(f([346, 29, 395, 188, 155, 109], 9401))
