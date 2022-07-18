132054db915ef2241703e8528f8674431441881f
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-07-18 17:58:02 -0400

    upd 518

--- DynamicProgramming/518.py
+++ DynamicProgramming/518.py
@@ -1,30 +1,79 @@
 class Solution:
     def change(self, amount: int, coins: list[int]) -> int:
-        # dp = [-1] * (amount + 1)
+        #######################################################################
+        # TC: O(N*M^2) SC: O(N*M)
 
-        # def f(amount: int, coin_i: int) -> int:
+        # _dp = [[0] * (amount + 1) for _ in range(len(coins))]
+
+        # def dp(coin_i: int, amount: int):
         #     if amount == 0:
         #         return 1
-        #     if amount < 0 or coin_i >= len(coins):
+        #     if coin_i < 0:
         #         return 0
+        #     return _dp[coin_i][amount]
 
-        #     if dp[amount] == -1:
-        #         dp[amount] = sum(
-        #             f(amount - i * coins[coin_i], coin_i + 1)
-        #             for i in range(amount // coins[coin_i] + 1)
-        #         )
+        # for coin_i, coin_info in enumerate(_dp):
+        #     for am in range(1, len(coin_info)):
+        #         for i in range(am // coins[coin_i] + 1):
+        #             _dp[coin_i][am] += dp(coin_i - 1, am - coins[coin_i] * i)
 
-        #     print(f'f({amount}, {coin_i}) = {dp[amount]}')
-        #     return dp[amount]
+        # return dp(len(coins) - 1, amount)
 
-        # return f(amount, 0)
+        #######################################################################
+        # TC: O(N*M) SC: O(N*M)
+
+        # NOT_PROCESSED = -1
+
+        # dp = [[NOT_PROCESSED] * (amount + 1) for _ in range(len(coins))]
+
+        # def f(coin_i: int, amount: int) -> int:
+        #     if amount == 0:
+        #         return 1
+        #     if coin_i < 0 or amount < 0:
+        #         return 0
+        #     x = dp[coin_i][amount]
+        #     if x == NOT_PROCESSED:
+        #         dont_take_this_coin = f(coin_i - 1, amount)
+        #         # take one of this coin type regardless of whether taken before
+        #         take_this_coin_once = f(coin_i, amount - coins[coin_i])
+        #         x = dp[coin_i][amount] = dont_take_this_coin + take_this_coin_once
+        #     return x
+
+        # return f(len(coins) - 1, amount)
+
+        #######################################################################
+        # TC: O(N*M) SC: O(N)
+
+        dp = [[0] * (amount + 1) for _ in range(2)]
+
+        for coin_info in dp:
+            coin_info[0] = 1
+
+        for i in range(1, amount // coins[0] + 1):
+            dp[0][coins[0] * i] = dp[0][coins[0] * (i - 1)]
+
+        for coin in coins[1:]:
+            for amount_ in range(1, amount + 1):
+                dont_take_this_coin = dp[0][amount_]
+                # take one of this coin type, regardless of whether taken before
+                take_this_coin_once = dp[1][amount_ - coin] if amount_ >= coin else 0
+                dp[1][amount_] = dont_take_this_coin + take_this_coin_once
+            dp[0] = dp[1]
+
+        return dp[min(len(coins)-1,1)][amount]
 
 
 g = Solution().change
 
 
-print(g(5, [1, 2, ]))
-# print(g(3, [2]))
-# print(g(10, [10]))
-# print(g(11, [10, 7, 1]))
-# print(g(500, [3, 5, 7, 8, 9, 10, 11]))
+print(
+    g(
+        5,
+        [1, 2, 5],
+    ),
+    4,
+)
+print(g(3, [2]), 0)
+print(g(10, [10]), 1)
+print(g(11, [10, 7, 1]), 3)
+print(g(500, [3, 5, 7, 8, 9, 10, 11]), 35502874)
