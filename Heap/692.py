468514796e716a89a34c28871a5791a485c5e8c8
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-07-30 14:04:01 -0400

    using minheap

--- Heap/692.py
+++ Heap/692.py
@@ -10,70 +10,65 @@ class ItemInfo:
 class Solution:
     def topKFrequent(self, words: list[str], k: int) -> list[str]:
         item_info: 'dict[str,ItemInfo]' = {}
-        heap: 'list[str]' = []
-        smallest_size = (k + 1) // 2
+        min_heap: 'list[str]' = []
 
         def cmp(l: str, r: str):
             l_freq, r_freq = item_info[l].freq, item_info[r].freq
-            return l_freq > r_freq or (l_freq == r_freq and l < r)
+            return l_freq < r_freq or (l_freq == r_freq and l < r)
 
-        def fixup(heap: 'list[str]', i: int):
-            parent_i = (i - 1) // 2
-            while i > 0 and cmp(heap[i], heap[parent_i]):
-                item_info[heap[i]].idx, item_info[heap[parent_i]].idx = parent_i, i
-                heap[i], heap[parent_i] = heap[parent_i], heap[i]
-                i = parent_i
-                parent_i = (i - 1) // 2
+        # def fixup(i: int):
+        #     parent_i = (i - 1) // 2
+        #     while i > 0 and cmp(min_heap[i], min_heap[parent_i]):
+        #         item_info[min_heap[i]].idx, item_info[min_heap[parent_i]].idx = parent_i, i
+        #         min_heap[i], min_heap[parent_i] = min_heap[parent_i], min_heap[i]
+        #         i = parent_i
+        #         parent_i = (i - 1) // 2
+
+        def fixdown(i: int):
+            while ...:
+                l = i * 2 + 1
+                if l >= len(min_heap):
+                    break
+                r = l + 1
+                if r < len(min_heap) and cmp(min_heap[r], min_heap[l]):
+                    l = r
+                # now [l] is the "smaller" child
+                if not cmp(min_heap[i], min_heap[l]):
+                    min_heap[i], min_heap[l] = min_heap[l], min_heap[i]
+                    i = l
+                else:
+                    break
 
         for word in words:
             try_insert = False
             if word not in item_info:
                 item_info[word] = ItemInfo(1)
-                if len(heap) < k:
-                    item_info[word].idx = len(heap)
-                    heap.append(word)
-                    fixup(heap, len(heap) - 1)
+                if len(min_heap) < k:
+                    item_info[word].idx = len(min_heap)
+                    min_heap.append(word)
+                    fixdown(len(min_heap) - 1)
                 else:
                     try_insert = True
             else:
                 item_info[word].freq += 1
                 if item_info[word].idx >= 0:
-                    fixup(heap, item_info[word].idx)
+                    fixdown(item_info[word].idx)
                 else:
                     try_insert = True
             if try_insert:
-                smallest_idx = smallest_size - 1
-                for i in range(smallest_size, len(heap)):
-                    if not cmp(heap[i], heap[smallest_idx]):
-                        smallest_idx = i
-                if cmp(word, heap[smallest_idx]):
-                    item_info[heap[smallest_idx]].idx = -1
-                    heap[smallest_idx] = word
-                    item_info[heap[smallest_idx]].idx = smallest_idx
-                    fixup(heap, smallest_idx)
+                if cmp(word, min_heap[0]):
+                    item_info[min_heap[0]].idx = -1
+                    min_heap[0] = word
+                    item_info[min_heap[0]].idx = 0
+                    fixdown(0)
 
-        def fixdown(heap: 'list[str]'):
-            i = 0
-            while ...:
-                l = i * 2 + 1
-                if l >= len(heap):
-                    break
-                r = l + 1
-                if r < len(heap) and cmp(heap[r], heap[l]):
-                    l = r
-                # now [l] is the "smaller" child
-                if not cmp(heap[i], heap[l]):
-                    heap[i], heap[l] = heap[l], heap[i]
-                    i = l
-                else:
-                    break
-
-        sorted_arr: list[str] = []
-        for _ in range(len(heap)):
-            sorted_arr.append(heap[0])
-            heap[0] = heap[-1]
-            del heap[-1]
-            fixdown(heap)
+        sorted_arr: list[str] = [''] * len(min_heap)
+        for i in range(len(sorted_arr)):
+            i = len(sorted_arr) - 1 - i
+            sorted_arr[i] = min_heap[0]
+            min_heap[0] = min_heap[-1]
+            del min_heap[-1]
+            fixdown(0)
 
         return sorted_arr
 
@@ -83,7 +78,6 @@ def test_1():
         "i",
         "love",
     ]
-
 
 def test_2():
     assert Solution().topKFrequent(
